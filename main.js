5041a3eca3b944faac8fd90fabebb75d43db58dd
esperanzzza <veronikatelychko@yahoo.com>
2017-04-19 15:12:58 +0300

    added validation for name, password and email

--- main.js
+++ main.js
@@ -1,88 +1,101 @@
 (function($, window, document) {
 
-// Listen for the jQuery ready event on the document
-$(function() {
+    // Listen for the jQuery ready event on the document
+    $(function() {
 
-     $('.js-validate-form').validateMe();
+        $('.js-validate-form').validateMe();
 
-});
-
-$.fn.validateMe = function() {
-    var fields = [];
-    var isValid = true;
-    var FIELD_TYPES = ['input','textarea','select'];
-    var RULES = {
-        'name' : {
-            regular: '^[a-zA-Z\-]*$',
-            errorMessage: 'Only latin letters',
-            minLength: 3,
-            maxLength: 20
-        }
-    }
-    var errorEl = $('<p>').addClass('error-message');
-
-
-
-    $(this).find(FIELD_TYPES.join(',')).each(function(){
-         // without buttons
-        if (($(this).attr('type') !== 'button') && ($(this).attr('type') !== 'submit')) {
-            fields.push(this);
-        }
     });
 
-    fields.forEach(function(field){
+    $.fn.validateMe = function() {
+        var fields = [];
+        var isValid = true;
+        var FIELD_TYPES = ['input', 'textarea', 'select'];
+        var RULES = {
+            'name': {
+                regular: '^[a-zA-Z\-]*$',
+                errorMessage: 'Only latin letters',
+                minLength: 3,
+                maxLength: 20
+            },
+            'password': {
+                regular: '([A-Za-z]+[0-9]|[0-9]+[A-Za-z])[A-Za-z0-9]*',
+                errorMessage: 'Include at least one letter and one number',
+                minLength: 8,
+                maxLength: 15
+            },
 
-        $(field).wrap( "<div class='field-group'></div>" );
+            'email': {
+                regular: '^[_a-zA-Z0-9\'-]+(\.[_a-zA-Z0-9\'-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(([0-9]{1,3})|([a-zA-Z]{2,3})|(name))$',
+                errorMessage: 'Not valid email address',
+                maxLength: 320
+            }
 
-        $(field).on('blur', function(){
-            console.log(checkLength(this) && checkReg(this));
+        }
+        var errorEl = $('<p>').addClass('error-message');
+
+
+
+        $(this).find(FIELD_TYPES.join(',')).each(function() {
+            // without buttons
+            if (($(this).attr('type') !== 'button') && ($(this).attr('type') !== 'submit')) {
+                fields.push(this);
+            }
         });
-    });
 
-    function checkLength(field) {
-        var fieldRuleBy = $(field).data('validme');
+        fields.forEach(function(field) {
 
-        var errorMessageMin = 'Less letters/symbols than ' + RULES[fieldRuleBy].minLength;
-        var errorMessageMax = 'More letters/symbols than ' + RULES[fieldRuleBy].maxLength;
+            $(field).wrap("<div class='field-group'></div>");
 
-        var errorElLength = errorEl.clone().addClass('error-message-length');
+            $(field).on('blur', function() {
+                console.log(checkLength(this) && checkReg(this));
+            });
+        });
 
-        $(field).parent().find('.error-message-length').remove();
+        function checkLength(field) {
+            var fieldRuleBy = $(field).data('validme');
 
-        if (field.value.length < RULES[fieldRuleBy].minLength) {
-            errorElLength.html(errorMessageMin).prependTo($(field).parent());
-            return false;
-        } else if (field.value.length > RULES[fieldRuleBy].maxLength) {
-            errorElLength.html(errorMessageMax).prependTo($(field).parent());
-            return false;
+            var errorMessageMin = 'Less letters/symbols than ' + RULES[fieldRuleBy].minLength;
+            var errorMessageMax = 'More letters/symbols than ' + RULES[fieldRuleBy].maxLength;
+
+            var errorElLength = errorEl.clone().addClass('error-message-length');
+
+            $(field).parent().find('.error-message-length').remove();
+
+            if (field.value.length < RULES[fieldRuleBy].minLength) {
+                errorElLength.html(errorMessageMin).prependTo($(field).parent());
+                return false;
+            } else if (field.value.length > RULES[fieldRuleBy].maxLength) {
+                errorElLength.html(errorMessageMax).prependTo($(field).parent());
+                return false;
+            }
+
+            return true;
         }
 
-        return true;
-    }
+        function checkReg(field) {
+            var errorElReg = errorEl.clone().addClass('error-message-reg');
 
-    function checkReg(field) {
-        var errorElReg = errorEl.clone().addClass('error-message-reg');
+            var fieldRuleBy = $(field).data('validme');
+            var errorMessageReg = RULES[fieldRuleBy].errorMessage;
 
-        var fieldRuleBy = $(field).data('validme');
-        var errorMessageReg = RULES[fieldRuleBy].errorMessage;
+            var result = field.value.match(RULES[fieldRuleBy].regular);
 
-        var result = field.value.match( RULES[fieldRuleBy].regular );
+            $(field).parent().find('.error-message-reg').remove();
 
-        $(field).parent().find('.error-message-reg').remove();
+            if (!result) {
+                errorElReg.html(errorMessageReg).prependTo($(field).parent());
+            }
 
-        if (!result) {
-            errorElReg.html(errorMessageReg).prependTo($(field).parent());
+            return !!result;
         }
 
-        return !!result;
-    }
+        // fields.forEach(function(field){
+        //     isValid = isValid && checkLength(field) && checkReg(field);
+        // });
 
-    // fields.forEach(function(field){
-    //     isValid = isValid && checkLength(field) && checkReg(field);
-    // });
+        // console.log(isValid);
 
-    // console.log(isValid);
-
-};
+    };
 
 }(window.jQuery, window, document));
